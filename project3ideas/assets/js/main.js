2cfa5eaf895d5b9e3ab7e0fedc29e5f5c7efec8b
marinayu1 <myu60@student.monash.edu>
2017-05-20 19:32:49 +1000

    added article 1

--- project3ideas/assets/js/main.js
+++ project3ideas/assets/js/main.js
@@ -4,38 +4,102 @@ $(document).ready(function() {
     $('.footnote').addClass('showme');
   });
 
-  $('.thumbnail').click(function() {
-    $('.this').toggleClass('enlarge');
+  $('.thumbnail').mouseenter(function() {
+    $('.transcript_info__image').addClass('showme');
   });
 
-
-$('.number').mouseleave(function() {
-   $('.footnote').removeClass('showme');
- });
-
- $('.metahaven').mouseenter(function() {
-   $('.cloud').addClass('enlarge');
- });
-
- $('.metahaven').mouseleave(function() {
-    $('.cloud').removeClass('enlarge');
-  });
-
-  $('.metahaven').mouseenter(function() {
-    $('.thecoming').addClass('enlarge');
-  });
-
-  $('.metahaven').mouseleave(function() {
-     $('.thecoming').removeClass('enlarge');
+  $('.thumbnail').mouseleave(function() {
+     $('.transcript_info__image').removeClass('showme');
    });
 
-  $('.thumbnail').click(function() {
-    $(this).toggleClass('enlarge');
-  });
+   $('.thumbnailsplit').mouseenter(function() {
+     $('.split_info__image').addClass('showme');
+   });
 
-  $('.link').click(function() {
-    $('.iframe').addClass('showme');
-      //run function that records clicks
-  });
+   $('.thumbnailsplit').mouseleave(function() {
+      $('.split_info__image').removeClass('showme');
+    });
 
-  
+    $('.thumbnailjoyless').mouseenter(function() {
+      $('.joyless_info__image').addClass('showme');
+    });
+
+    $('.thumbnailjoyless').mouseleave(function() {
+       $('.joyless_info__image').removeClass('showme');
+     });
+
+     $('.thumbnailwakeup').mouseenter(function() {
+       $('.wakeup_info__image').addClass('showme');
+     });
+
+     $('.thumbnailwakeup').mouseleave(function() {
+        $('.wakeup_info__image').removeClass('showme');
+      });
+
+      $('.thumbnailonline').mouseenter(function() {
+        $('.online_info__image').addClass('showme');
+      });
+
+      $('.thumbnailonline').mouseleave(function() {
+         $('.online_info__image').removeClass('showme');
+       });
+
+
+
+
+
+
+  $('.computer').mouseenter(function() {
+     $('.computer__image').addClass('showme');
+   });
+
+   $('.computer').mouseleave(function() {
+      $('.computer__image').removeClass('showme');
+    });
+
+    $('.record').mouseenter(function() {
+       $('.record__image').addClass('showme');
+     });
+
+     $('.record').mouseleave(function() {
+        $('.record__image').removeClass('showme');
+      });
+    $('.transcript').mouseenter(function() {
+       $('.').addClass('showme');
+     });
+
+     $('.singlekey').mouseenter(function() {
+        $('.singlekey__image').addClass('showme');
+      });
+
+
+      $('.singlekey').mouseleave(function() {
+         $('.singlekey__image').removeClass('showme');
+       });
+
+       $('.computer').mouseenter(function() {
+          $('.computer__image').addClass('showme');
+        });
+
+
+        $('.computer').mouseleave(function() {
+           $('.computer__image').removeClass('showme');
+         });
+
+
+
+           $('.AFRAID').mouseenter(function() {
+              $('.AFRAID__image').addClass('showme');
+            });
+
+            $('.AFRAID').mouseleave(function() {
+               $('.AFRAID__image').removeClass('showme');
+             });
+
+
+
+
+
+
+
+      });
