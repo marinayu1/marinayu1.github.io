02a8278f0b91df14a7b5c00d2673ba125419201e
marinayu1 <myu60@student.monash.edu>
2017-05-22 23:27:14 +1000

    added 3 articles

--- project3ideas/assets/js/main.js
+++ project3ideas/assets/js/main.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
 
-  $('.number').mouseenter(function() {
-    $('.footnote').addClass('showme');
-  });
 
   $('.thumbnail').mouseenter(function() {
     $('.transcript_info__image').addClass('showme');
@@ -44,7 +41,45 @@ $(document).ready(function() {
          $('.online_info__image').removeClass('showme');
        });
 
+       $('.thumbnailbuilding').mouseenter(function() {
+         $('.building_info__image').addClass('showme');
+       });
 
+       $('.thumbnailbuilding').mouseleave(function() {
+          $('.building_info__image').removeClass('showme');
+        });
+
+        $('.thumbnailtruism').mouseenter(function() {
+          $('.truism_info__image').addClass('showme');
+        });
+
+        $('.thumbnailtruism').mouseleave(function() {
+           $('.truism_info__image').removeClass('showme');
+         });
+
+         $('.thumbnailyourself').mouseenter(function() {
+           $('.yourself_info__image').addClass('showme');
+         });
+
+         $('.thumbnailyourself').mouseleave(function() {
+            $('.yourself_info__image').removeClass('showme');
+          });
+
+          $('.thumbnailoffline').mouseenter(function() {
+            $('.offline_info__image').addClass('showme');
+          });
+
+          $('.thumbnailoffline').mouseleave(function() {
+             $('.offline_info__image').removeClass('showme');
+           });
+
+           $('.thumbnailrealname').mouseenter(function() {
+             $('.realname_info__image').addClass('showme');
+           });
+
+           $('.thumbnailrealname').mouseleave(function() {
+              $('.realname_info__image').removeClass('showme');
+            });
 
 
 
@@ -53,9 +88,11 @@ $(document).ready(function() {
      $('.computer__image').addClass('showme');
    });
 
-   $('.computer').mouseleave(function() {
-      $('.computer__image').removeClass('showme');
+   $('.computer__image').click(function() {
+      $('this').removeClass('showme');
     });
+
+
 
     $('.record').mouseenter(function() {
        $('.record__image').addClass('showme');
@@ -77,14 +114,9 @@ $(document).ready(function() {
          $('.singlekey__image').removeClass('showme');
        });
 
-       $('.computer').mouseenter(function() {
-          $('.computer__image').addClass('showme');
-        });
+       
 
 
-        $('.computer').mouseleave(function() {
-           $('.computer__image').removeClass('showme');
-         });
 
 
 
@@ -95,6 +127,90 @@ $(document).ready(function() {
             $('.AFRAID').mouseleave(function() {
                $('.AFRAID__image').removeClass('showme');
              });
+
+/* googlesearch*/
+$('.vague').mouseenter(function() {
+$('.vague__image').addClass('showme');
+});
+
+$('.vague').mouseleave(function() {
+$('.vague__image').removeClass('showme');
+});
+
+$('.memories').mouseenter(function() {
+$('.memories__image').addClass('showme');
+});
+
+$('.memories').mouseleave(function() {
+$('.memories__image').removeClass('showme');
+});
+
+$('.recall').mouseenter(function() {
+$('.recall__image').addClass('showme');
+});
+
+$('.recall').mouseleave(function() {
+$('.recall__image').removeClass('showme');
+});
+
+$('.rememeber').mouseenter(function() {
+$('.rememeber__image').addClass('showme');
+});
+
+$('.rememeber').mouseleave(function() {
+$('.rememeber__image').removeClass('showme');
+});
+
+$('.folklore').mouseenter(function() {
+$('.folklore__image').addClass('showme');
+});
+
+$('.folklore').mouseleave(function() {
+$('.folklore__image').removeClass('showme');
+});
+
+$('.clobbering').mouseenter(function() {
+$('.clobbering__image').addClass('showme');
+});
+
+$('.clobbering').mouseleave(function() {
+$('.clobbering__image').removeClass('showme');
+});
+
+$('.dangerous').mouseenter(function() {
+$('.dangerous__image').addClass('showme');
+});
+
+$('.dangerous').mouseleave(function() {
+$('.dangerous__image').removeClass('showme');
+});
+
+$('.percolate').mouseenter(function() {
+$('.percolate__image').addClass('showme');
+});
+
+$('.percolate').mouseleave(function() {
+$('.percolate__image').removeClass('showme');
+});
+
+$('.integers').mouseenter(function() {
+$('.integers__image').addClass('showme');
+});
+
+$('.integers').mouseleave(function() {
+$('.integers__image').removeClass('showme');
+});
+
+$('.representing').mouseenter(function() {
+$('.representing__image').addClass('showme');
+});
+
+$('.representing').mouseleave(function() {
+$('.representing__image').removeClass('showme');
+});
+
+
+
 
 
 
