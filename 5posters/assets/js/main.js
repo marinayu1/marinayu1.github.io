51e7e785525e49cac0b4a8621d7c180226d2ca09
marinayu1 <myu60@student.monash.edu>
2017-08-18 09:11:12 +1000

    Add posters

--- 5posters/assets/js/main.js
+++ 5posters/assets/js/main.js
@@ -1,3 +1,44 @@
 $(document).ready(function(){
 
+  setTimeout(function () {
+    $('.description').addClass('addred');
+  }, 1000);
+
+
+  $('.felix').mouseenter(function() {
+       $('.felixdates').addClass('showme');
+     });
+
+     $('.felix').mouseleave(function() {
+          $('.felixdates').removeClass('showme');
+        });
+
+        $('.posterone').mouseenter(function() {
+        $('.posterone__image').addClass('showme');
+        });
+
+        var div = $('.autoscroller');
+
+        $('.autoscroller').bind('scroll mousedown wheel DOMMouseScroll mousewheel keyup', function(evt) {
+            if (evt.type === 'DOMMouseScroll' || evt.type === 'keyup' || evt.type === 'mousewheel') {
+
+            }
+            if (evt.originalEvent.detail < 0 || (evt.originalEvent.wheelDelta && evt.originalEvent.wheelDelta > 0)) {
+                clearInterval(autoscroller);
+            }
+            if (evt.originalEvent.detail > 0 || (evt.originalEvent.wheelDelta && evt.originalEvent.wheelDelta < 0)) {
+                clearInterval(autoscroller);
+            }
+        });
+
+        var autoscroller = setInterval(function(){
+            var pos = div.scrollTop();
+            if ((div.scrollTop() + div.innerHeight()) >= div[0].scrollHeight) {
+                clearInterval(autoscroller);
+            }
+            div.scrollTop(pos + 1);
+        }, 25);
+
+
+
 });
