b843ae49d4e3accb770f29ee941d6452b13a6243
marinayu1 <myu60@student.monash.edu>
2017-08-18 12:20:33 +1000

    finishing

--- 5posters/assets/js/main.js
+++ 5posters/assets/js/main.js
@@ -2,7 +2,40 @@ $(document).ready(function(){
 
   setTimeout(function () {
     $('.description').addClass('addred');
-  }, 1000);
+  }, 5000);
+
+
+
+  setTimeout(function () {
+      $('.whiterectangle').addClass('moveleft');
+    }, 1000);
+
+    setTimeout(function () {
+        $('.whiterectangle').addClass('movedown');
+      }, 3000);
+
+      setTimeout(function () {
+          $('.whiterectangle').addClass('moveright');
+        }, 5000);
+
+        setTimeout(function () {
+            $('.whiterectangle').addClass('movedown2');
+          }, 7000);
+
+          setTimeout(function () {
+              $('.whiterectangle').addClass('moveleft');
+            }, 9000);
+
+
+  setTimeout(function () {
+            $('.numbers').addClass('enlarge');
+          }, 2000);
+
+          setTimeout(function () {
+                    $('.numbers').addClass('smaller');
+                  }, 4000);
+
+
 
 
   $('.felix').mouseenter(function() {
@@ -13,31 +46,102 @@ $(document).ready(function(){
           $('.felixdates').removeClass('showme');
         });
 
+
+        $('.pierre').mouseenter(function() {
+             $('.pierredates').addClass('showme');
+           });
+
+           $('.pierre').mouseleave(function() {
+                $('.pierredates').removeClass('showme');
+              });
+
+              $('.don').mouseenter(function() {
+                   $('.dondates').addClass('showme');
+                 });
+
+                 $('.don').mouseleave(function() {
+                      $('.dondates').removeClass('showme');
+                    });
+
+                    $('.her').mouseenter(function() {
+                         $('.herdates').addClass('showme');
+                       });
+
+                       $('.her').mouseleave(function() {
+                            $('.herdates').removeClass('showme');
+                          });
+
+                          $('.piet').mouseenter(function() {
+                               $('.pietdates').addClass('showme');
+                             });
+
+                             $('.piet').mouseleave(function() {
+                                  $('.pietdates').removeClass('showme');
+                                });
         $('.posterone').mouseenter(function() {
         $('.posterone__image').addClass('showme');
         });
 
-        var div = $('.autoscroller');
-
-        $('.autoscroller').bind('scroll mousedown wheel DOMMouseScroll mousewheel keyup', function(evt) {
-            if (evt.type === 'DOMMouseScroll' || evt.type === 'keyup' || evt.type === 'mousewheel') {
-
-            }
-            if (evt.originalEvent.detail < 0 || (evt.originalEvent.wheelDelta && evt.originalEvent.wheelDelta > 0)) {
-                clearInterval(autoscroller);
-            }
-            if (evt.originalEvent.detail > 0 || (evt.originalEvent.wheelDelta && evt.originalEvent.wheelDelta < 0)) {
-                clearInterval(autoscroller);
-            }
+        $('.posterone').mouseleave(function() {
+        $('.posterone__image').removeClass('showme');
         });
 
-        var autoscroller = setInterval(function(){
-            var pos = div.scrollTop();
-            if ((div.scrollTop() + div.innerHeight()) >= div[0].scrollHeight) {
-                clearInterval(autoscroller);
-            }
-            div.scrollTop(pos + 1);
-        }, 25);
+        $('.postertwo').mouseenter(function() {
+        $('.postertwo__image').addClass('showme');
+        });
+
+        $('.postertwo').mouseleave(function() {
+        $('.postertwo__image').removeClass('showme');
+        });
+
+        $('.posterthree').mouseenter(function() {
+        $('.posterthree__image').addClass('showme');
+        });
+
+        $('.posterthree').mouseleave(function() {
+        $('.posterthree__image').removeClass('showme');
+        });
+
+        $('.posterfour').mouseenter(function() {
+        $('.posterfour__image').addClass('showme');
+        });
+
+        $('.posterfour').mouseleave(function() {
+        $('.posterfour__image').removeClass('showme');
+        });
+
+        $('.posterfive').mouseenter(function() {
+        $('.posterfive__image').addClass('showme');
+        });
+
+        $('.posterfive').mouseleave(function() {
+        $('.posterfive__image').removeClass('showme');
+        });
+
+
+
+        var audioBell = document.createElement('audio');
+             audioBell.setAttribute('src', 'file:///Users/marinayu/Downloads/French%20Telephone%20Numbers%20Part%201.mp3');
+             $.get();
+             audioBell.addEventListener("load", function() {
+               audioBell.play();
+             }, true);
+
+             $('.number').mouseenter(
+                  function() {
+                    audioBell.play();
+                },
+              );
+
+
+              $('.number').mouseenter(function() {
+                   $('.backgroundnumbers').addClass('showme');
+                 });
+
+                 $('.number').mouseleave(function() {
+                      $('.backgroundnumbers').removeClass('showme');
+                    });
+
 
 
 
